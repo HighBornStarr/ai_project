6b1a865b22717e84e70f75cac90d2de19b6e2db9
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2023-01-13 20:46:18 -0600

    Start of AI task

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>ChatGPT is a powerful language model developed by OpenAI. It is trained on a large dataset of human conversation and text, allowing it to generate human-like responses to a wide range of prompts.</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access to visit in last the 24 hours.</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} alt="ai" />
+    </div>
+  </div>
+);
+
+export default Header;
