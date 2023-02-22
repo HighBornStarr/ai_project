733c2318a59afe16edeb7526a7585a4805b3d642
Alex Starr <116136090+HighBornStarr@users.noreply.github.com>
2023-02-21 19:57:51 -0600

    Article start

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -13,13 +13,15 @@ We are blogging about it.</h1>
     </div>
     <div className='gpt3__blog-container'>
     <div className='gpt3__blog-container_groupA'>
-   <Article imgUrl={blog01}/>
+   <Article imgUrl={blog01} date="Sep 26, 2021"title=
+     "GPT-3 and Open AI is the future. Let us explore how it is?"
+   />
     </div>
     <div className='gpt3__blog-container_groupB'>
-    <Article imgUrl={blog02}/>
-    <Article imgUrl={blog03}/>
-    <Article imgUrl={blog04}/>
-    <Article imgUrl={blog05}/>
+    <Article imgUrl={blog02} date="Sep 26, 2021"title="Let us begin. The future is within our grasp."/>
+    <Article imgUrl={blog03} date="Sep 26, 2021"title="GPT-3 and Open AI are the best tools?"/>
+    <Article imgUrl={blog04} date="Sep 26, 2021"title="Investing in tech?, walk with us"/>
+    <Article imgUrl={blog05} date="Sep 26, 2021"title="How big is your imagination?"/>
     </div>
     </div>
     </div>
